b3f563036eee2091a58f4fdab91de4632dcd8130
Artiom Ciobanu <aciobanumain@gmail.com>
2021-01-26 22:27:40 +0200

    .

--- Lab1/Models/AircraftTreeCell.cs
+++ Lab1/Models/AircraftTreeCell.cs
@@ -2,6 +2,15 @@
 {
     public class AircraftTreeCell
     {
-        
+        public Aircraft Value { get; set; }
+
+        public Aircraft Left { get; set; }
+        public Aircraft Right { get; set; }
+
+        public static explicit operator Aircraft(AircraftTreeCell aircraftTreeCell)
+            => aircraftTreeCell.Value;
+
+        public override string ToString()
+            => ((Aircraft) this).ToString();
     }
 }
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,19 +5,19 @@ using Newtonsoft.Json;
 
 namespace Lab1
 {
-    class Program
+    internal class Program
     {
-        static Aircraft[] GetAircraftsFromFile(string fileName)
+        private static Aircraft[] GetAircraftsFromFile(string fileName)
         {
             var fileText = File.ReadAllText(fileName);
 
             return JsonConvert.DeserializeObject<Aircraft[]>(fileText);
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Aircraft[] aircrafts = GetAircraftsFromFile("Aircrafts.json");
-            
+            var aircrafts = GetAircraftsFromFile("Aircrafts.json");
+
             var aircraftById = aircrafts.FindWhere(aircraft => aircraft.Id == 5);
             var yak40 = aircrafts.FindByName("Yakovlev 40");
 
