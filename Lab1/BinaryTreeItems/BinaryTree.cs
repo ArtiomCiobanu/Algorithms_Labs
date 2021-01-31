696a288531dd3ff14458e3c576fd4127fbcffa05
Artiom Ciobanu <aciobanumain@gmail.com>
2021-01-31 22:19:47 +0200

    Binary tree classes

---
+++ Lab1/BinaryTreeItems/BinaryTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1.BinaryTreeItems
+{
+    public class BinaryTree
+    {
+        public BinaryTreeNode RootNode { get; set; }
+
+        public void Insert(BinaryTreeNode node)
+        {
+            var currentNode = RootNode;
+
+            while (true)
+            {
+                if (currentNode.Value.Price > node.Value.Price)
+                {
+                    currentNode = currentNode.Right;
+                }
+                else if (currentNode.Value.Price > node.Value.Price)
+                {
+                    currentNode = currentNode.Right;
+                }
+                else
+                {
+                    Console.WriteLine("Already exists");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Lab1/BinaryTreeItems/BinaryTreeNode.cs
@@ -0,0 +1,14 @@
+﻿using Lab1.Models;
+
+namespace Lab1.BinaryTreeItems
+{
+    public class BinaryTreeNode
+    {
+        public Aircraft Value { get; set; }
+
+        public BinaryTreeNode Left { get; set; }
+        public BinaryTreeNode Right { get; set; }
+
+        public override string ToString() => Value.ToString();
+    }
+}
