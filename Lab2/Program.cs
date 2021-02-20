bc5caabf5835de2867ce46b399785f8fec35c349
Artiom Ciobanu <aciobanumain@gmail.com>
2021-02-20 11:15:01 +0200

    Added Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Lab1.Models;
+using Newtonsoft.Json;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        private static Aircraft[] GetAircraftsFromFile(string fileName)
+        {
+            var fileText = File.ReadAllText(fileName);
+
+            return JsonConvert.DeserializeObject<Aircraft[]>(fileText);
+        }
+
+        private static void Main(string[] args)
+        {
+            var aircrafts = GetAircraftsFromFile("Aircrafts.json");
+        }
+    }
+}
